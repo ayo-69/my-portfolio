8284516bcf212747b0cc2d4e7f3cc8bf407d84c4
Isaac Nommoh Hayab <isaachayab0@gmail.com>
2025-10-21 09:12:16 +0100

    Added emailer for the portfolio

---
+++ emailer/server.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message, budget } = req.body;
+
+    // Create a transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com', // Replace with your SMTP server
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'isaachayab0@gmail.com', // Replace with your email
+            pass: 'mqjt ywtz xpig eueq' // Replace with your password
+        }
+    });
+
+    // Setup email data
+    let mailOptions = {
+        from: '"Your Name" <your-email@gmail.com>', // sender address
+        to: 'isaachayab0@gmail.com', // list of receivers
+        subject: 'New Portoflio Contact Form Submission', // Subject line
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nBudget: ${budget}`, // plain text body
+        html: `<b>Name:</b> ${name}<br><b>Email:</b> ${email}<br><b>Message:</b> ${message}<br><b>Budget:</b> ${budget}` // html body
+    };
+
+    // Send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+            res.status(500).send('Something went wrong.');
+        }
+        console.log('Message sent: %s', info.messageId);
+        res.status(200).send('Email sent successfully.');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
